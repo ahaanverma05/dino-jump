e77cf54b3f558c66159fc8d0b18ccb1740d036ea
ahaanverma05 <81346836+ahaanverma05@users.noreply.github.com>
2021-05-19 13:20:12 +0400

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
+var abc;
+var buiverfuyyu = document.getElementById("jumpbtn");
 function gamestarted() {
-var score = 0;
+buiverfuyyu.addEventListener("click", (e)=> {
+    jump();
+})
 var dino = document.getElementById("dino");
 var cactus = document.getElementById("cactus");
-cactus.classList.add("blablabla");
+setInterval(cactus.classList.add("blablabla"), 1000);
 function jump() {
     if(dino.classList != "jumpclass") {
     dino.classList.add("jumpclass");
@@ -11,13 +15,15 @@ function jump() {
     },350)
 }
 }
+
 var isAlive = setInterval(function() {
     let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue("top"));
     let cactusLeft = parseInt(window.getComputedStyle(cactus).getPropertyValue("left"));
-    if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){ 
+    if(cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
     document.getElementById("gameovertext").innerHTML="Game Over.";
     }
 }, 10)
+
 document.addEventListener("keydown", (e)=> {
     jump()
 })
